288d5f01ae8e370acc7accd2b6024b23f7cb1cc0
Carlos Bedoya <bedoyacarlosalberto@gmail.com>
2024-10-26 13:44:44 -0500

    Add scripts mysql

--- src/scripts/script.sql
+++ src/scripts/script.sql
@@ -1,29 +1,33 @@
-CREATE TABLE `app-inter`.`estudiantes` (
-  `id` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `materia_id` INT NOT NULL,
-  `profesor_id` INT NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE DATABASE `app-inter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `estudiantes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `cedula` varchar(255) DEFAULT NULL,
+  `materia_id` int NOT NULL,
+  `profesor_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `app-inter`.`materias` (
-  `id` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `creditos` INT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `materias` (
+  `id` int NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `creditos` int DEFAULT NULL,
+  `id_materia1` int NOT NULL,
+  `id_materia2` int NOT NULL,
+  `nombres` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `app-inter`.`profesores` (
-  `Id` INT NOT NULL,
-  `Nombres` VARCHAR(45) NOT NULL,
-  `id_Materia1` INT NOT NULL,
-  `id_Materia2` INT NOT NULL,
-  PRIMARY KEY (`Id`));
+CREATE TABLE `profesores` (
+  `Id` int NOT NULL,
+  `nombres` varchar(255) DEFAULT NULL,
+  `id_Materia1` int NOT NULL,
+  `id_Materia2` int NOT NULL,
+  PRIMARY KEY (`Id`,`id_Materia1`,`id_Materia2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `app-inter`.`programas` (
-  `Id` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `creditos` INT NOT NULL,
-  PRIMARY KEY (`Id`));
 
 
 insert into `app-inter`.materias values (1,'Biologia', 3)
@@ -38,8 +42,8 @@ insert into `app-inter`.materias values (9,'Dibujo', 1)
 insert into `app-inter`.materias values (10,'Religion', 1)
 
 
-insert into `app-inter`.profesores values (1,'Carlos',1,3)
-insert into `app-inter`.profesores values (2,'Pedro',4,6)
-insert into `app-inter`.profesores values (3,'Raul',5,3)
-insert into `app-inter`.profesores values (4,'Paola',6,1)
-insert into `app-inter`.profesores values (5,'Luisa',2,4)
+insert into `app-inter`.profesores values (1,'Carlos',1,2)
+insert into `app-inter`.profesores values (2,'Pedro',5,6)
+insert into `app-inter`.profesores values (3,'Raul',9,10)
+insert into `app-inter`.profesores values (4,'Paola',3,4)
+insert into `app-inter`.profesores values (5,'Luisa',7,8)
